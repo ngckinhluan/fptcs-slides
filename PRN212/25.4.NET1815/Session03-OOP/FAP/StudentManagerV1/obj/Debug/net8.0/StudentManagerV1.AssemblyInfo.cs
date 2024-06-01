d71a24d43fa2df3a568ed3bd96c29cfdff78e4b5
Trần Ngọc Kinh Luân <nkluantran2907@gmail.com>
2024-06-01 18:31:19 +0700

    update some materials

--- PRN212/25.4.NET1815/Session03-OOP/FAP/StudentManagerV1/obj/Debug/net8.0/StudentManagerV1.AssemblyInfo.cs
+++ PRN212/25.4.NET1815/Session03-OOP/FAP/StudentManagerV1/obj/Debug/net8.0/StudentManagerV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagerV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456c061b950448743ecfd7cf4824fc65a2bc728c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagerV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagerV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
